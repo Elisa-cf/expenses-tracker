1a68af3104d157fc44b40987529e1007a692a25b
Elisa <elisacanyelles@gmail.com>
2021-11-30 22:26:11 +0100

    created app and expenses component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>294.59€</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
